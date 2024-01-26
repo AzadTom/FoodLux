acf5c6e755d1ce5a77ef36a80afc557bf990ca81
azadtom <kumarazad2917@gmail.com>
2024-01-26 23:52:51 +0530

    modified:backend

--- backend/controllers/paymentController.js
+++ backend/controllers/paymentController.js
@@ -52,12 +52,12 @@ export const paymentVerification  = async(req,res)=>{
 
          const cart  = await cartModel.find({user:req.userid});
 
-         const subtotal = cart.reduce((acc, item) => ((item.price) * (item.qty)) + acc, 0);
+        //  const subtotal = cart.reduce((acc, item) => ((item.price) * (item.qty)) + acc, 0);
 
          const order = await OrderModel.create({
             user:req.userid,
             items:cart,
-            price: subtotal,
+            price: 5000,
             paymentDetail:{
                 username: name,
                 useremail:email,
