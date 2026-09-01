d35735d1be6590594968902583a37956dc0756a8
azadtom <kumarazad2917@gmail.com>
2026-09-01 13:13:56 +0530

    feat: update import paths for PrismaService in order, product, and cart services

--- backend2/src/order/order.service.ts
+++ backend2/src/order/order.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class OrderService {
