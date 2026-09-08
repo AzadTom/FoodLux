fbc99c4e5671cee9d439fa7c6cb6880198539ccf
azadtom <kumarazad2917@gmail.com>
2026-09-08 22:46:30 +0530

    feat: update BASE_URL3 to point to the live backend server

--- frontend/src/services/service.js
+++ frontend/src/services/service.js
@@ -86,7 +86,7 @@ export const getUserProfile = async () => {
 // Products & Categories
 const Local3 = "http://localhost:3001";
 const Live3 = "https://backend2-seven-beta.vercel.app";
-export const BASE_URL3 = Local3;
+export const BASE_URL3 = Live3;
 
 
 const api2 = axios.create({
